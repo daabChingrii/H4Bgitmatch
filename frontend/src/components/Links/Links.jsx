20d8acf32b043aca62fb561b13c3640e8092c946
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-07-10 13:48:36 +0530

    modified:   frontend/.gitignore

---
+++ frontend/src/components/Links/Links.jsx
@@ -0,0 +1,11 @@
+export const GHshuf = "https://github.com/ShubhangiXD";
+export const TWTshuf = "https://twitter.com/renyxx_twt";
+export const LIshuf = "https://www.linkedin.com/in/shubhangidutta/";
+
+export const TWTpraj = "https://twitter.com/prajzisclueless";
+export const LIpraj = "https://www.linkedin.com/in/prajukta-dey-a24252239/";
+export const GHpraj = "https://github.com/prajuktadey";
+
+export const GHdiya = "https://github.com/MDas08";
+export const LIdiya = "https://www.linkedin.com/in/diya-das-mdas/";
+export const TWTdiya = "https://twitter.com/Error418_MDas";
